e6d9fa798e2e71764b1b7d778348be14e8ba4d0a
Carlos Granell <carlos.granell.canut@gmail.com>
2018-01-30 19:04:35 +0100

    update table grouping

--- 02_preprocessing-data.R
+++ 02_preprocessing-data.R
@@ -12,20 +12,37 @@ p_load(
   "lubridate",
   "highcharter",
   "knitr",
-  "kableExtra"
+  "kableExtra", 
+  "scales"
 )
 
 file_name <- "datos-practica-uji.csv"
 data_path <- here::here("data", file_name)
 
 tb <- read_csv(data_path)
-head(tb, 8)
+head(nrow(tb))
 
 
 tb <- tb %>%
-  mutate(date = as.Date(fecha, format = "%d/%m/%Y"),
-         year = year(ymd(date)),
-         duracion_media = mean (duracion))
+  mutate(date = lubridate::dmy(fecha), 
+           # date= as.Date(fecha, format = "%d/%m/%Y"),
+         year = year(date)) %>%
+  select(-fecha)
+tb
+
+
+dur_year <- tb %>%
+  group_by(year) %>%
+  summarise( 
+    count = n(),
+    dur_media = mean(duracion, na.rm = TRUE),
+    dur1_per = sum(duracion_1) / sum(duracion),
+    dur2_per = sum(duracion_2) / sum(duracion),
+    dur3_per = sum(duracion_3) / sum(duracion)
+    )
+
+scales::percent(dur_year$dur1_per)
+
 
 # https://cran.r-project.org/web/packages/kableExtra/README.html
 kable(tb %>% select(-duracion_1, -duracion_2, -duracion_3, -chicos, -chicas, -conocimiento, -lugar, -date, -duracion_media),
