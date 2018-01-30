4bb2e4a7d3af28850d5e4bf6ff2b01009fb008c5
Carlos Granell <carlos.granell.canut@gmail.com>
2018-01-31 00:04:54 +0100

    Added stacked bar

--- 02_preprocessing-data.R
+++ 02_preprocessing-data.R
@@ -13,7 +13,8 @@ p_load(
   "highcharter",
   "knitr",
   "kableExtra", 
-  "scales"
+  "scales",
+  "viridis"
 )
 
 file_name <- "datos-practica-uji.csv"
@@ -28,30 +29,53 @@ tb <- tb %>%
            # date= as.Date(fecha, format = "%d/%m/%Y"),
          anyo = year(fecha_final)) %>%
   select(-fecha) %>%
-  rename(dur_total = duracion, dur1 = duracion_1, dur2 = duracion_2, dur3 = duracion_3) %>%
+  rename(num_sesion = id, dur_total = duracion, dur1 = duracion_1, dur2 = duracion_2, dur3 = duracion_3) %>%
   arrange(fecha_final)
 
-tb <- tb %>%
-  mutate(dur1_per = dur1 / dur_total,
-         dur2_per = dur2 / dur_total,
-         dur3_per = dur3 / dur_total)
-         # date= as.Date(fecha, format = "%d/%m/%Y"),
-tb
+kable(tb %>% select(num_sesion, dur_total, alumnos, cursos, conocimiento))
+# https://cran.r-project.org/web/packages/kableExtra/README.html
+kable(tb %>% select(num_sesion, dur_total, alumnos, cursos, conocimiento),
+      format = "latex", 
+      booktabs = TRUE, 
+      caption = "Sesiones") %>%
+  kable_styling(full_width = T)
+#kable_styling(latex_options = "scale_down")
+#kable_styling(latex_options = c("striped", "hold_position"), full_width = FALSE)
 
-scales::percent(tb$dur1_per)
+tb_dur <- tb %>%
+  # mutate(dur1_per = dur1 / dur_total,
+  #        dur2_per = dur2 / dur_total,
+  #        dur3_per = dur3 / dur_total) %>%
+  select(-alumnos, -chicos, -chicas, -conocimiento, -cursos, -lugar) %>%
+  select(num_sesion, fecha_final, anyo, everything())
+         # date= as.Date(fecha, format = "%d/%m/%Y"),
+
+tb_dur2 <- tb_dur %>%
+  gather(dur1, dur2, dur3, key = "tipo_seccion", value = "dur_seccion") %>%
+  arrange(fecha_final, tipo_seccion)
+
+tb_dur2 <- tb_dur2 %>%
+  mutate (dur_per = dur_seccion / dur_total * 100)
+
+
+# http://t-redactyl.io/blog/2016/01/creating-plots-in-r-using-ggplot2-part-4-stacked-bar-plots.html
+ggplot(tb_dur2, aes(x=num_sesion, y=dur_per)) + 
+  geom_bar(aes(fill = tipo_seccion), stat = "identity") +
+  labs(x = "Sesiones", y="Duración (min)") +
+  scale_fill_viridis(discrete=TRUE, option="viridis") + theme_bw() +
+  coord_flip()
+
+
+
+#####################3
+
+
+scales::percent(tb_dur$dur1_per)
 
 
 ggplot(evaldata_years_long, aes(year, value)) +
   geom_bar(aes(fill = variable), stat = "identity") +
   ylab("mean value") +
-
-ggplot(tb, aes(x=id, y=dur_total)) + 
-  geom_bar(aes(fill = anyo), stat = "identity") +
-  labs(x = "Sesiones", y="Duración (min)") +
-  theme_bw()
-
-
-ggplot(tb, )
 
 dur_year <- tb %>%
   group_by(year) %>%
@@ -63,7 +87,7 @@ dur_year <- tb %>%
     dur3_per = sum(duracion_3) / sum(duracion)
     )
 
-scales::percent(dur_year$dur1_per)
+
 
 
 # https://cran.r-project.org/web/packages/kableExtra/README.html
