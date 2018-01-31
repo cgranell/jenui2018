54a1d6958c289d85e987a8d90bbb99d02a3834a0
cgranell <carlos.granell.canut@gmail.com>
2018-01-31 13:05:58 +0100

    Updated stacked bar chart

--- 02_preprocessing-data.R
+++ 02_preprocessing-data.R
@@ -55,34 +55,62 @@ kable(tb_table,
                 latex_options = c("striped","hold_position"))
 
 
-# Temporal distribtion of each section per session
+# Temporal distribution of each section per session
 tb_dur <- tb %>%
-  select(-alumnos, -chicos, -chicas, -conocimiento, -cursos, -lugar) %>%
+  select(-chicos, -chicas, -conocimiento, -cursos, -lugar) %>%
   select(num_sesion, fecha_final, anyo, everything())
 
 tb_dur <- tb_dur %>%
-  gather(dur1, dur2, dur3, key = "tipo_seccion", value = "dur_seccion") %>%
-  arrange(fecha_final, tipo_seccion)
-tb_dur
+  gather(dur1, dur2, dur3, key = "tipo_seccion", value = "dur_seccion")
 
-tb_dur2 <- tb_dur %>%
-  mutate (dur_per = dur_seccion / dur_total * 100)
+tb_dur <- tb_dur %>%
+  mutate (porcentaje = round(dur_seccion / dur_total, 2) * 100)
 
-tb_dur2
+media_alumnos <- mean(tb_dur$alumnos)  
+tb_dur <- tb_dur %>%
+  group_by(num_sesion)  %>%
+  # mutate (pos = cumsum(porcentaje) - (0.5 * porcentaje)) %>%
+  mutate (asistencia = ifelse(alumnos > media_alumnos, "mayor", "menor")) %>%
+  arrange(desc(fecha_final), tipo_seccion)
+  
+tb_dur$asistencia <- factor(tb_dur$asistencia, level = unique(tb_dur$asistencia),
+                              labels = c("Superior media", "Inferior media"))
+
+cols_asistencia <- c("Superior media" = "#a6611a", "Inferior media" = "#dfc27d")
+
+
+tb_dur$tipo_seccion <- factor(tb_dur$tipo_seccion, level = unique(tb_dur$tipo_seccion),
+                               labels = c("Explicación profesor", "Proyecto guiado", "Proyecto libre"))
+
 
 # http://t-redactyl.io/blog/2016/01/creating-plots-in-r-using-ggplot2-part-4-stacked-bar-plots.html
-ggplot(tb_dur2, aes(x=num_sesion, y=dur_per)) + 
-  geom_bar(aes(fill = tipo_seccion), stat = "identity") +
-  labs(x = "Sesiones", y="Duración (min)") +
-  scale_fill_viridis(discrete=TRUE, option="viridis") + theme_bw() +
-  coord_flip()
+ggplot(tb_dur, aes(x=num_sesion, y=porcentaje)) + 
+  geom_bar(aes(fill = tipo_seccion), stat = "identity", position= position_stack(reverse = TRUE)) +
+  geom_point(aes(x=num_sesion, y=alumnos, colour=asistencia)) +
+  scale_fill_brewer(palette = "Set3") + theme_bw() +
+  scale_colour_manual(values = cols_asistencia) +
+  geom_text(aes(x=num_sesion, y=alumnos, label = alumnos), size=2, vjust = 1, nudge_y = -1) +
+  geom_text(aes(x=num_sesion, y=porcentaje, label = ifelse(porcentaje>0, paste(porcentaje, "%"), "")), 
+            position = position_stack(vjust = 0.5)) +
+  theme(legend.position="bottom", 
+        legend.direction="horizontal",
+        legend.title = element_blank()) +
+  # coord_flip() +
+  scale_y_continuous(labels = dollar_format(suffix = "%", prefix="")) +
+  labs(x = "Sesiones (año - número)", y="Porcentaje (%)") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  ggtitle("Ditribución temporal de secciones por sesión (%)")
+  
+
+# Save stacked bar chart
+file_name <- "porcentaje_secciones.png"
+data_path <- here::here("figs", file_name)
+ggsave(data_path, dpi = 600)
+
 
 
 
 #####################3
-
-
-scales::percent(tb_dur$dur1_per)
 
 
 ggplot(evaldata_years_long, aes(year, value)) +
